62913a87d431731b2eab6ac9c9be1b9f108c3768
Sora Tomita <sora.tomita083@gmail.com>
2019-07-11 23:51:46 +0900

    微修正

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # chat-demo
 
 ### TODO
-* 初期データ登録
-* DB接続部分の作成
+* ログイン部分の改修
 
 #### Reference
 * [SpringBootでチャットアプリを作る](https://qiita.com/duke-gonorego/items/7700ada96bf3804b19e3)
---
+++ src/test/java/com/sora/socket/web/chatdemo/service/ChatServiceTest.java
@@ -0,0 +1,16 @@
+package com.sora.socket.web.chatdemo.service;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ChatServiceTest {
+
+    @Autowired
+    private ChatService chatService;
+
+    @Test
+    public void test() {
+        //var list = chatService.loadPastMessage();
+    }
+
+}
